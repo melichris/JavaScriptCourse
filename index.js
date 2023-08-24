fea6f5930ee5e26c765fa42381ff255cdefaa4b8
Melichris <melichristian51@gmail.com>
2023-08-24 19:11:52 +0100

    updates

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ let age=21
 console.log("hello "+name+". Your age is :"+age)
 
 document.getElementById('name').innerHTML="hello "+fname+" You are "+age+" year old."
-
+// operators
 let students=21
 students+=1
 console.log(students)
+// accepting data from user through windpw prompt
+let name= window.prompt("what is your name ?")
