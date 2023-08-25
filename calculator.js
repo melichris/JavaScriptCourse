3ed220fb421dc1b8be5a5c12f3260027e4419f4d
Melichris <melichristian51@gmail.com>
2023-08-25 15:14:52 +0100

    calculator v1.1

--- calculator.js
+++ calculator.js
@@ -1,30 +1,30 @@
 let a, b, sum, quotien, product, diff, operand;
 document.getElementById("myButton").onclick = function () {
-  a = document.getElementById("num1");
-  b = document.getElementById("num2");
-  operand = document.getElementById("operator");
-
-  sum = a + b;
-  product = a * b;
-  diff = a - b;
-  quotien = a / b;
+  a = document.getElementById("num1").value;
+  a=Number(a)
+  b = document.getElementById("num2").value;
+  b=Number(b)
+  operand = document.getElementById("operator").value;
+  operand=Number(operand)
 
   if (operand == 1) {
     sum = a + b;
     document.getElementById("results").innerHTML = "Your results is :" + sum;
-  }else if (operand == 2) {
+  } else if (operand == 2) {
     diff = a - b;
     document.getElementById("results").innerHTML = "Your results is :" + diff;
-  }else if (operand == 3) {
+  } else if (operand == 3) {
     product = a * b;
-    document.getElementById("results").innerHTML = "Your results is :" + product;
-  }else if (operand == 3) {
-    if(a>b){
-    quotien = a / b;
-    document.getElementById("results").innerHTML = "Your results is :" + quotien;
-  }else{
-    document.getElementById("results").innerHTML = "Your results is undefined";
+    document.getElementById("results").innerHTML =
+      "Your results is :" + product;
+  } else if (operand == 3) {
+    if (a > b) {
+      quotien = a / b;
+      document.getElementById("results").innerHTML =
+        "Your results is :" + quotien;
+    } else {
+      document.getElementById("results").innerHTML =
+        "Your results is undefined";
+    }
   }
-}
-
 };
