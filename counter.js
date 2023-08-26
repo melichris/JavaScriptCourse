007e7e1a71654db2a10c2d8916ad8fced12e8e6d
Melichris <melichristian51@gmail.com>
2023-08-26 23:19:29 +0100

    update counter

--- counter.js
+++ counter.js
@@ -1,9 +1,14 @@
 let count;
-document.getElementById("decrease").onclick=function{
-    count-=1
-    document.getElementById("myResults").innerHTML=count
-}
-document.getElementById("increase").onclick=function{
-    count=0
-    document.getElementById("myResults").innerHTML=count
-}
+count = Number(count);
+document.getElementById("decrease").onclick = function () {
+  count -= 1;
+  document.getElementById("myResults").innerHTML = count;
+};
+document.getElementById("reset").onclick = function () {
+  count = 0;
+  document.getElementById("myResults").innerHTML = count;
+};
+document.getElementById("increase").onclick = function () {
+  count += 1;
+  document.getElementById("myResults").innerHTML = count;
+};
