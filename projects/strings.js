67af1b28e19fffaff210599c5fa69c07834d5b64
Melichris <melichristian51@gmail.com>
2023-08-30 17:04:01 +0100

    #

---
+++ projects/strings.js
@@ -0,0 +1,5 @@
+// Complete the function getCharCount such that it returns the number of characters in the str parameter that it receives.
+function getCharCount(str) {
+  return str.length;
+}
+console.log(getCharCount("christian"));
