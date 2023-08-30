0ffa2a41775ae72c7047fbd9e2d11537bb7ce50e
Melichris <melichristian51@gmail.com>
2023-08-30 17:05:27 +0100

    #

--- projects/strings.js
+++ projects/strings.js
@@ -3,3 +3,13 @@ function getCharCount(str) {
   return str.length;
 }
 console.log(getCharCount("christian"));
+
+// Complete the function shoutMyName such that it returns the name parameter it receives all in upper case
+function shoutMyName(name) {
+  return name.toUpperCase();
+}
+
+// Sample usage - do not modify
+console.log(shoutMyName("Sam")); // "SAM"
+console.log(shoutMyName("Charley")); // "CHARLEY"
+console.log(shoutMyName("alex")); // "ALEX"
