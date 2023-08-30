100c9f002fad452da70577c973e2e4058c0f0265
Melichris <melichristian51@gmail.com>
2023-08-30 14:38:30 +0100

    converting from number to string

--- JSCourse/2Numbers.js
+++ JSCourse/2Numbers.js
@@ -1,11 +1,22 @@
 // numbers are variable values whihc do not take qoutes ("")
 //  to declare a number we use variable declairation in variables.js
+
 let age = 12; //this is a number
 let phone = "675679853"; // this is a string
-// displaying their datatypes
-console.log(typeof age);
-console.log(typeof phone);
-// typeof will be explained further
+
+////// displaying their datatypes
+function displayDataTypes() {
+  console.log(typeof age);
+  console.log(typeof phone);
+}
+
+//NB: typeof will be explained further
 
 // convertion
-
+// converting from number to string we use the x.toString() where x is the variable name
+function numberToString(age) {
+  return age.toString(); //converts age to a string
+}
+console.log(numberToString(age))
+// view datatype of varaible
+console.log(typeof numberToString(age))
