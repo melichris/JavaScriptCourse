e0e14f374251ee117340ee83f077cd99e23b7b0a
Melichris <melichristian51@gmail.com>
2023-08-30 13:55:03 +0100

    variables

---
+++ JSCourse/variables.js
@@ -0,0 +1,13 @@
+// variables definition
+// let use to define a variable which value changes
+//  const use to define variables with fixed values
+let fname;
+fname = "chris";
+const id = 123;
+// diplaying in a console
+console.log(fname);
+console.log(id);
+fname = "boris";
+id = 21; //line gives and error
+console.log(fname);
+console.log(id);
