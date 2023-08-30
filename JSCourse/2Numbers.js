eec10b421640be3f0daf4a86662e16c912dddeaf
Melichris <melichristian51@gmail.com>
2023-08-30 16:49:48 +0100

    2 numbers

--- JSCourse/2Numbers.js
+++ JSCourse/2Numbers.js
@@ -3,6 +3,8 @@
 
 let age = 12; //this is a number
 let phone = "675679853"; // this is a string
+let x=3.1
+let y=3.56
 
 ////// displaying their datatypes
 function displayDataTypes() {
@@ -14,18 +16,34 @@ function displayDataTypes() {
 
 // convertion
 // 1.converting from number to string we use the x.toString() where x is the variable name
-function numberToString(age) {
+function numberToString() {
   return age.toString(); //converts age to a string
 }
 // view datatype of varaible
-console.log(typeof numberToString(age));
+console.log(typeof numberToString());
 
 // 2.converting from string to interger
-function stringToNumber(phone) {
+function stringToNumber() {
   return parseInt(phone);
 }
 // view datatype of varaible
-console.log(typeof stringToNumber(phone));
+console.log(typeof stringToNumber());
 
-// converting 
+// strings to number based on radix
+function stringToNumber2() {
+  return Number.parseInt(phone, 2); //where "2" is the radix in this case binary
+}
+console.log(stringToNumber2());
 
+// number methods
+// roundUp
+function roundUp(){
+return Math.round(y)
+}
+console.log(roundUp())
+
+// roundDown
+function roundDown(){
+    return Math.floor(y)
+}
+console.log(roundDown())
---
+++ projects/numbers.js
@@ -0,0 +1,5 @@
+//Create a function getNextAge such that it returns the age next year (by adding 1 to the current age).
+function getNextAge(age){
+    return parseInt(age)+1
+}
+console.log(getNextAge(21))
