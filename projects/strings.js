e8b288da8e695de348808972f68af45f52501cd1
Melichris <melichristian51@gmail.com>
2023-08-30 17:07:58 +0100

    first character

--- projects/strings.js
+++ projects/strings.js
@@ -22,3 +22,12 @@ function lowerName(name) {
 // Sample usage - do not modify
 console.log(lowerName("Sam")); // "sam"
 console.log(lowerName("ALEX")); // "a
+
+// Complete the function getFirstCharacter such that it returns the first character of the name parameter it receives
+function getFirstCharacter(name) {
+  return name[0];
+}
+
+// Sample usage - do not modify
+console.log(getFirstCharacter("Amsterdam")); // "A"
+console.log(getFirstCharacter("Japan")); // "J"
