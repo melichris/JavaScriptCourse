6fc1b62770bf8e67489a72e4bfd88e206cc74faa
Melichris <melichristian51@gmail.com>
2023-08-30 17:08:45 +0100

    get last character

--- projects/strings.js
+++ projects/strings.js
@@ -31,3 +31,13 @@ function getFirstCharacter(name) {
 // Sample usage - do not modify
 console.log(getFirstCharacter("Amsterdam")); // "A"
 console.log(getFirstCharacter("Japan")); // "J"
+
+// Complete the function getLastCharacter such that it returns the last character of the name parameter it receives.
+function getLastCharacter(name) {
+  return name[name.length - 1];
+}
+
+// Sample usage - do not modify
+console.log(getLastCharacter("Sam")); // "m"
+console.log(getLastCharacter("Alex")); // "x"
+console.log(getLastCharacter("Charley")); // "y"
